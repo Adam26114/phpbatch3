b1c60889ecff029dc576a23552d5b7e6d5e004de
Adam26114 <zakato26114@gmail.com>
2023-01-20 18:27:29 +0630

    20 Jan 2023

---
+++ part1/l1echo.php
@@ -0,0 +1,63 @@
+<?php
+
+
+// phpinfo();
+
+
+// *** php မှာ sentance ဆုံးတိုင်း semi column ;  ပိတ်ပေး ရပါမယ် 
+
+// comment ပိတ်တာ forword slash နှစ်ချောင်း ( same as javascript)
+// php က case sensative ဖြစ်တယ်
+echo "Hello Myanmar <br/>";     // print ထုတ်မယ်ဆိုရင် echo "" or '' သုံးပေးရပါမယ်;
+echo "Hello Thailand <br/>";    // html code သိပါတယ်
+echo ("Hello Indonesia <br/>");
+echo ("Hello Serilanka <hr/>");
+
+
+echo "Hello Myanmar",'I love Myanamr';      // multi parameta ရပါတယ်
+// echo ("Hello Myanmar",'I love Myanamr'); //error multi parameta ရေးမယ်ဆိုရင် () ထဲထည့်မရေးရပါဝူး
+echo "<hr/>";
+print "Hello Philippine <hr/>";
+// print "Hello Philippine","Hello Thailand";
+print "Hello Thailand ";
+// print ("Hello Philippine","Hello Thailand"); //print က multi parameta မရပါဝူး
+
+echo "<hr/>";   // *** back tip `` နဲ့ရေးလို့မရဝုး 
+echo 'hay';
+echo '<br/>';
+echo 500; 
+echo '<br/>';
+echo (300); 
+echo '<br/>';
+echo 456.23; 
+echo true;    //resul =  1
+echo '<br/>';
+echo false;   //resul = empty
+echo '<br/>';
+echo null;   //resul = empty
+echo '<br/>';
+
+//Special Escape character (\)
+echo "He's my brother";
+echo '<br/>';
+print "He's my sister";
+echo '<br/>';
+
+echo 'He\'s my brother';
+echo '<br/>';
+print 'She\'s my sister';
+echo '<br/>';
+
+echo 'He\'s my brother,he\'s a doctor';
+echo '<br/>';
+print 'She\'s my sister,she\'s a doctor';
+echo '<br/>';
+
+?>
+
+
+
+
+
+<!-- php file က C:\xampp\htdocs ထဲမှာ ရှိရမယ်  -->
+<!-- http://localhost/phpbatch3/part1/l1echo.php -->
