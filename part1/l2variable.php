97daa929de494aaa79887629c8ed577621b1c183
Adam26114 <zakato26114@gmail.com>
2023-01-20 18:44:15 +0630

    20 jab 2023

--- part1/l2variable.php
+++ part1/l2variable.php
@@ -2,6 +2,22 @@
 
 // Variables
 
+// variable ကြေညာရင် $ ကို သုံးရမယ်  
+$fullname = "Zwe Aung Naing";
+$job = "Developer";
 
+echo $fullname;
+echo $job;
+echo "$fullname"; // double code က ပေးလိုက်တဲ့  variable ဘာသာပြန်ပေးနိုင်တယ် 
+echo '$fullname'; // single code က ရေးတဲ့ အတိုင်း ဘဲ‌ပေါ်တယ်
+echo `$job`; // back tip က ဘာမှ မပေါ်ဝူး
+
+echo "<br/>"; 
+
+echo "My name is    $fullname"; // result = My name is Zwe Aung Naing
+echo "<br/>"; 
+echo "My name is $(fullname)"; // result = My name is Zwe Aung Naing
+echo "<br/>"; 
+echo 'My name is $(fullname)'; // result = My name is ${fullname}
 
 ?>
