cbb723cabe148a00db2e1a42108685a427c08604
Adam26114 <zakato26114@gmail.com>
2023-01-27 18:32:10 +0630

    26 Jan 2022

---
+++ part1/l6ifstatment.php
@@ -0,0 +1,27 @@
+<?php
+
+// if statement
+
+$x = 10;
+$y = 20;
+
+if($x > $y){
+    echo "Your Condition is yes";
+}
+
+if($x < $y){
+    echo "Your Condition is yes";
+}
+
+echo "<br/>";
+
+//if else
+if($x > $y){
+    echo "Yes x greater than y";
+}else{
+    echo "No, x less than y";
+}
+
+?>
+
+<!-- 27IF -->
