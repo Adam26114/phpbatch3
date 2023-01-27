54b725721eaac27a6157f97e23b6e53bcc0fa0cf
Adam26114 <zakato26114@gmail.com>
2023-01-27 17:44:30 +0630

    26 Jan 2023

---
+++ part1/l4assignmentoperator.php
@@ -0,0 +1,26 @@
+<?php
+
+// Assign ( = ) 
+
+$city = "";
+$city = NULL; // Declare
+$city = "Yangon"; // Assign or Initialize
+$city = "Mandalay"; // Overwrite or Reassign
+
+
+echo $city;
+echo "<br/>";
+
+$x = 100;
+$y = 200;
+
+// echo $x; //result = 100
+// $x = $y
+// echo $x; //result = 200
+
+
+
+$x = $y + $x;
+echo $x; //result = 300
+
+?>
