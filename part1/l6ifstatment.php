6d8e89ea0192b6b34fd7437fb57e675d7e35ac74
Adam26114 <zakato26114@gmail.com>
2023-02-03 17:16:21 +0630

    3 feb 2023

--- part1/l6ifstatment.php
+++ part1/l6ifstatment.php
@@ -22,6 +22,111 @@ if($x > $y){
     echo "No, x less than y";
 }
 
+echo "<br/>";
+
+
+// ? Shorthand
+
+if($x < $y) echo "Yes x less than y"; else echo "No, x greater than y";
+
+echo "<br/>";
+
+if($x < $y){
+    echo "Yes,x less than y";
+}else{
+    echo "No, x greater than y";
+}
+
+echo "<br/>";
+
+$color = 'gray';
+$colorvalue = $color; // Assign by reference
+
+// echo $color;
+// echo $colorvalue;
+
+// if ($colorvalue == "red"){
+//     echo "My fav color is red.";
+// }else{
+//     echo "I hate red color.";
+// }
+
+
+
+// if ($colorvalue == "red"){
+//     echo "My fav color is $color.";
+// }else{
+//     echo "I hate $color color.";
+// }
+
+
+// ? concat (.)
+if ($colorvalue == "red"){
+    echo "My fav color is ".$color;
+}else{
+    echo "I hate ".$color." color.";
+}
+
+echo "<hr/>";
+
+
+// ? Logical Operators
+
+//  &&And) ||(Or) !(Not)
+
+$a = 100;
+$b = 200;
+$c = 300;
+
+if($a < $b && $b > $c){
+    echo "It is true.";
+}else{
+    echo "It is false.";
+}
+
+echo "<br/>";
+
+if($a < $b && $b < $c){
+    echo "It is true.";
+}else{
+    echo "It is false.";
+}
+
+echo "<br/>";
+
+if($a < $b || $b < $c){
+    echo "It is true.";
+}else{
+    echo "It is false.";
+}
+
+echo "<br/>";
+
+if($a < $b || $b > $c && $a < $c){
+    echo "It is true.";
+}else{
+    echo "It is false.";
+}
+
+echo "<br/>";
+
+//  true       false     false
+
+if($a < $b || $b > $c && $a < $c){
+    echo "It is true.";
+}else{
+    echo "It is false.";
+}
+
+echo "<hr/>";
+
+if(!($a < $b)){
+    echo "It is true.";
+}else{
+    echo "It is false.";
+}
+
+
 ?>
 
 <!-- 27IF -->
